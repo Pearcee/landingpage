fa26964f2f372cef817d0974a1a1d67c1741021b
Steven Pearce <sjpearce@gmail.com>
2022-12-07 09:08:30 +0000

    12/07/2022 09:08:29

---
+++ html.ps1
@@ -0,0 +1,52 @@
+<#
+5769d2b88391e40899f2b4bc41087c13
+npm install 
+# Edit server.js and set your DarkSky API key.
+Run node server
+
+MyWebsite/
+|--css/
+|  |--materialize.css
+|
+|--fonts/
+|  |--roboto/
+|
+|--js/
+|  |--materialize.js
+|
+|--index.html
+
+git clone https://github.com/pearcee/fuzzy-lamp.git
+git clone https://github.com/googlecodelabs/your-first-pwapp.git
+git clone https://github.com/Pearcee/your-first-pwapp.git
+git clone https://github.com/Pearcee/pwapp.git
+git clone https://github.com/Pearcee/pwa.rocks.git
+git clone https://github.com/zuixjs/zuix-html-pwa
+
+echo "# fuzzy-lamp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Pearcee/fuzzy-lamp.git
+git remote add origin https://github.com/Pearcee/WeatherPWA.git
+git push -u origin master
+
+git pull 
+$project = 'my-project'
+cp "index.html" "$project\index.html"
+md $project
+cd $project
+$files = "css\my.scss", "fonts/roboto", "js\my.ts", "js/scripts.js", "css/styles.css"
+New-Item -ItemType "file" -Path $files -force
+
+git remote add origin https://github.com/Pearcee/WeatherPWA.git
+git push -u origin master
+
+
+#>
+
+$comment = get-date
+git status
+git add --all .
+git commit -m $comment
+git push -u origin master
